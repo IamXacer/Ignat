2f2c16f01a88a498d9bffed60fda291b8b428fcd
Phantom <phanton7487@gmail.com>
2023-11-21 22:58:47 +0200

    Д5

--- src/s2-homeworks/4/h4/HW4.tsx
+++ src/s2-homeworks/4/h4/HW4.tsx
@@ -16,6 +16,8 @@ function HW4() {
         }
     }
 
+
+
     const [checked, setChecked] = useState<boolean>(false)
     const testOnChange = (e: ChangeEvent<HTMLInputElement>) =>
         setChecked(e.currentTarget.checked)
